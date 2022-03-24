76bd6022b348aed2d0a3c517a913804c27833658
Brian Darmi <brian.darmi@gmail.com>
2022-03-24 10:45:26 +0800

    pannellum implementation

--- js/index.js
+++ js/index.js
@@ -2,11 +2,11 @@
   const response = await fetch('https://api.github.com/repos/zynchromatik/three-js-experiments/contents/');
   const data = await response.json();
   let htmlString = '<ul>';
-  for (let file of data) {
+  for (const file of data) {
     if (file.name !== 'index.html' && file.name.split('.').pop() == 'html') {
-      htmlString += `<li><a href="${file.path}">${file.name.split(".")[0]}</a></li>`;
+      htmlString += `<li><a href="${file.path}">${file.name.split('.')[0]}</a></li>`;
     }
   }
   htmlString += '</ul>';
   document.getElementsByTagName('div')[0].innerHTML = htmlString;
-})()
+})();
