5724fc9895b128f2164a2b2371f3e8669b40080f
Brian Darmi <brian.darmi@gmail.com>
2022-03-29 10:52:00 +0800

    removed archviewer

--- js/view_loader.js
+++ js/view_loader.js
@@ -2,22 +2,23 @@
 no-undef,no-unused-vars,prefer-destructuring,global-require,import/no-dynamic-require
 */
 function loadView(viewName) {
-  fetch(`./views/${viewName}/data.json`).then((r) => r.json()).then((json) => {
+  // Loads views of a folder based on data.json file inside it
+  const viewFolder = `./views/${viewName}`;
+  fetch(`${viewFolder}/data.json`).then((r) => r.json()).then((json) => {
     // Default settings
     let props = {
       autoLoad: true,
       compass: false,
-      showZoomCtrl: false,
-      keyboardZoom: false,
+      showControls: false,
       autoRotate: -2,
       autoRotateInactivityDelay: 30000,
       default: {
         firstScene: json.firstScene,
-        author: 'Archipelago',
         sceneFadeDuration: 1000,
       },
       scenes: {},
     };
+
     // Special case for partial panorama settings (non-full 360°)
     if (json.type === 'partial') {
       props = {
@@ -28,6 +29,7 @@ function loadView(viewName) {
         avoidShowingBackground: true,
       };
     } else props.type = json.type;
+
     // Loading scenes
     Object.keys(json.scenes).forEach((scene) => {
       const objScene = json.scenes[scene];
@@ -36,42 +38,60 @@ function loadView(viewName) {
         title: objScene.text,
         pitch: objScene.dir[0],
         yaw: objScene.dir[1],
-        panorama: `./views/archipelago-bali/${scene}.jpg`,
+        panorama: `${viewFolder}/${scene}.jpg`,
         hotSpots: [],
       };
 
       // Loading Hotspots: Scenes
-      Object.keys(objScene.scenes).forEach((spot) => {
-        const objSpot = objScene.scenes[spot];
-        props.scenes[scene].hotSpots.push({
-          type: 'scene',
-          cssClass: 'sceneSpot',
-          text: json.scenes[spot].text,
-          sceneId: spot,
-          pitch: objSpot[0],
-          yaw: objSpot[1],
+      if (typeof objScene.scenes !== 'undefined') {
+        Object.keys(objScene.scenes).forEach((spot) => {
+          const objSpot = objScene.scenes[spot];
+          props.scenes[scene].hotSpots.push({
+            type: 'scene',
+            cssClass: 'spot sceneSpot',
+            text: json.scenes[spot].text,
+            sceneId: spot,
+            pitch: objSpot[0],
+            yaw: objSpot[1],
+          });
         });
-      });
+      }
 
       // Loading Hotspots: Infos
-      Object.keys(objScene.infos).forEach((spot) => {
-        const objSpot = objScene.infos[spot];
-        props.scenes[scene].hotSpots.push({
-          type: 'info',
-          // cssClass: 'infoSpot',
-          text: objSpot.text,
-          pitch: objSpot.pos[0],
-          yaw: objSpot.pos[1],
+      if (typeof objScene.infos !== 'undefined') {
+        Object.keys(objScene.infos).forEach((spot) => {
+          const objSpot = objScene.infos[spot];
+          props.scenes[scene].hotSpots.push({
+            type: 'info',
+            cssClass: 'spot infoSpot',
+            text: objSpot.text,
+            pitch: objSpot.pos[0],
+            yaw: objSpot.pos[1],
+          });
         });
-      });
+      }
+
+      // Loading Hotspots: Links
+      if (typeof objScene.links !== 'undefined') {
+        Object.keys(objScene.links).forEach((spot) => {
+          const objSpot = objScene.links[spot];
+          props.scenes[scene].hotSpots.push({
+            type: 'info',
+            cssClass: 'spot linkSpot',
+            text: objSpot.text,
+            pitch: objSpot.pos[0],
+            yaw: objSpot.pos[1],
+            URL: objSpot.url,
+          });
+        });
+      }
     });
 
     const viewer = pannellum.viewer('panorama', props);
-    /*
+
     viewer.on('mousedown', (event) => {
       const coords = viewer.mouseEventToCoords(event);
       console.log(coords);
     });
-    */
   });
 }
